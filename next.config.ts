38c51255c8c05ec61b52626e70fa42c93f890b41
Pheonix2852 <sagnikkumbhakar.2003.va@gmail.com>
2025-07-10 16:48:30 +0530

    .

--- next.config.ts
+++ next.config.ts
@@ -2,12 +2,10 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   eslint: {
-    // Warning: This allows production builds to successfully complete even if
-    // your project has ESLint errors.
     ignoreDuringBuilds: true,
   },
   images: {
-    domains: ["randomuser.me"],
+    domains: ["randomuser.me"], // ✅ allow external image domain
   },
 };
 
