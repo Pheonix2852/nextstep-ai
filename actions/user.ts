e7aa59bbfc0e9c0bbc994112879f111b3ebc7d47
Pheonix2852 <sagnikkumbhakar.2003.va@gmail.com>
2025-07-15 14:44:31 +0530

    Updated schema unique constraints and increased database creation transaction time

--- actions/user.ts
+++ actions/user.ts
@@ -64,7 +64,7 @@ export async function updateUser(data: UpdateUserData) {
                 return { updatedUser, industryInsight }
 
             }, {
-                timeout: 10000, // default 5000
+                timeout: 30000, // default 5000
             }
         );
 
